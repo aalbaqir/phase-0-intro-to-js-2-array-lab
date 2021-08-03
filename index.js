200002015967854f59d94e5454684daa953dac16
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-08-03 00:12:50 -0500

    commit tree c8f5b6bd0cfb128b3dfffdec577ec9a19db22822

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+    return cats
+}   
+function appendCat(){
+    cats.slice(0, 3)
+    return cats
+}
+  
+// function prependCat(){
+//     cats.pop("Arnold")
+// }
